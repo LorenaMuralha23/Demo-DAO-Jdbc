27a8ea0b1074c6823228889c7a22d4a2ad230e4d
Lorena Muralha <mlmuralha2301@gmail.com>
2023-10-31 19:47:58 -0300

    findByDepartment implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -96,6 +99,51 @@ public class SellerDaoJDBC implements SellerDao{
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department dp) {
+		PreparedStatement st = null;
+		ResultSet rs = null; //ele aponta, de forma padrão, para a posição zero. Ao executar uma query, o objeto retornado vai para a posição 1
+		try {
+			
+			st = conn.prepareStatement("SELECT seller.*, department.Name as DepName"
+					+ " FROM seller INNER JOIN department"
+					+ " ON seller.DepartmentId = department.Id"
+					+ " WHERE DepartmentId = ?"
+					+ " ORDER BY Name");
+			
+			st.setInt(1, dp.getId());
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<Seller>();
+			Map<Integer, Department> map = new HashMap<Integer, Department>();
+			
+			while (rs.next()) {//com a explicação lá de cima, entendemos que esse next é feito para observar se existe uma posição 1. Isso é, se algum obj foi retornado :)
+				
+				//retornará falso caso na lista do map não existir o departamento
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				if (dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				
+				Seller obj = instantiateSeller(rs, dep);				
+				list.add(obj);
+			}
+			
+			return list;
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} 
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+		
 	}
 
 }
